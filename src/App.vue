67e50f5081a26be432e9e645fed081c1a64d2f5b
hinsenoo <837545254@qq.com>
2020-02-11 19:41:46 +0800

    CORS\JSONP 跨域

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import axios from 'axios'
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      data: ''
+    }
+  },
+  mounted(){
+    let url="https://www.imooc.com/u/loading";
+    // axios.get(url).then(()=>{});
+    jsonp(url,(err,res)=>{
+      let result = res;
+      this.data = result;
+      console.log(result);
+    })
   }
 }
 </script>
