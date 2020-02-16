6b6d41bbb84db1af215a4ffc8927fc968293f607
hinsenoo <837545254@qq.com>
2020-02-16 23:59:50 +0800

    NavHeader、NavFooter 组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        nav-footer
+    <div class="footer">
+        <div class="footer-logo">
+            <img src="/imgs/logo-footer.png" alt="">
+            <p>小米商城</p>
+        </div>
+        <div class="footer-link">
+            <a href="javascript:;" target="_blank">github主页</a><span>|</span>
+            <a href="javascript:;" target="_blank">掘金主页</a><span>|</span>
+            <a href="javascript:;" target="_blank">全栈项目</a><span>|</span>
+            <a href="javascript:;" target="_blank">Vue项目</a>
+        </div>
+        <div class="copyright">Copyright ©2019 <span class="domain">hinsenoo</span> All Rights Reserved.</div>
     </div>
 </template>
 <script>
@@ -10,3 +20,38 @@
         
     }
 </script>
+<style lang="scss">
+    @import './../assets/scss/config.scss';
+    .footer{
+        height: 234px;
+        border-top: 4px solid $colorA;
+        background-color: $colorB;
+        color: $colorD;
+        font-size: $fontI;
+        text-align: center;
+        .footer-logo{
+            margin-top: 46px;
+            margin-bottom: 31px;
+            img{
+                width: 53px;
+                height: 36px;
+                margin-bottom: 13px;
+            }
+        }
+        .footer-link{
+            a{
+                color: $colorD;
+                display: inline-block;
+            }
+            span{
+                margin:0 10px;
+            }
+            margin-bottom: 13px;
+        }
+        .copyright{
+            .domain{
+                color: $colorA;
+            }
+        }
+    }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -7,9 +7,9 @@ module.exports = {
             // 拦截 /api 的访问
             // 例：https://www.imooc.com/u/loading
             // 当访问到 /u 时会转发到 target
-            '/common': {
+            '/api': {
                 // 代理的目标地址
-                target: 'https://www.imooc.com',
+                target: 'http://mall-pre.springboot.cn',
                 // 是否要将主机头的源点更改为 url 地址
                 changeOrigin: true,
                 // 路径转发规则：把 /api 置为空
