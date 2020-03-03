630b6ed62169ec92b0262a124c1bb5c20d4b9a30
hinsenoo <837545254@qq.com>
2020-03-04 02:11:39 +0800

    购物车渲染

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -40,13 +40,16 @@
         }
         .title{
             float: left;
-            color: #333333;
             margin-left: 54px;
-            font-weight: bold;
+            h2{
+                font-size:28px;
+                color:#333333;
+            }
             span{
                 font-size: 14px;
                 margin-left: 17px;
                 color: #999999;
+                font-weight:200;
             }
         }
         .username{
