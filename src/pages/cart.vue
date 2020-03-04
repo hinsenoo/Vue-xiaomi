a0fe128dc7cc9b19972daf0252403d26a370c4e8
hinsenoo <837545254@qq.com>
2020-03-05 01:32:01 +0800

    购物车更新和删除

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -22,7 +22,7 @@
                     <ul class="cart-item-list">
                         <li class="cart-item" v-for="(item,index) in list" v-bind:key="index ">
                             <div class="item-check">
-                                <span class="checkbox" :class="{'checked': item.productSelected}"></span>
+                                <span class="checkbox" :class="{'checked': item.productSelected}" @click="updateCart(item)"></span>
                             </div>
                             <div class="item-name">
                                 <img v-lazy="item.productMainImage" alt="">
@@ -31,13 +31,13 @@
                             <div class="item-price">{{item.productPrice}}元</div>
                             <div class="item-num">
                                 <div class="num-box">
-                                    <a href="javascript:;">-</a>
+                                    <a href="javascript:;" @click="updateCart(item,'-')">-</a>
                                     <span>{{item.quantity}}</span>
-                                    <a href="javascript:;">+</a>
+                                    <a href="javascript:;" @click="updateCart(item,'+')">+</a>
                                 </div>
                             </div>
                             <div class="item-total">{{item.productTotalPrice}}元</div>
-                            <div class="item-del"></div>
+                            <div class="item-del" @click="delProduct(item)"></div>
                         </li>
                     </ul>
                 </div>
@@ -80,11 +80,48 @@
             this.getCartList();
         },
         methods:{
+            // 获取购物车列表
             getCartList(){
                 this.axios.get('/carts').then((res)=>{
                     this.renderData(res);
                 })
             },
+            // 更新购物车数量和购物车单选状态
+            updateCart(item,type){
+                // 商品数量
+                let quantity = item.quantity;
+                // 受否选中
+                let selected = item.productSelected;
+                if(type == '-'){
+                    if(quantity == 1){
+                        alert('商品至少保留一件');
+                        return;
+                    }
+                    --quantity;
+                }else if(type == '+'){
+                    if(quantity >= item.productStock){
+                        alert('购买的商品不能超过库存数量');
+                        return;
+                    }
+                    ++quantity;
+                }else{
+                    selected = !item.productSelected;
+                }
+                this.axios.put(`/carts/${item.productId}`,{
+                    quantity,
+                    selected
+                }).then((res)=>{
+                    this.renderData(res);
+                })
+            },
+            //删除购物车商品 （单条）
+            delProduct(item){
+                this.axios.delete(`/carts/${item.productId}`).then((res)=>{
+                    this.renderData(res);
+                });
+                // To: 删除弹框，确认后再发送请求
+            }
+            ,
             // 全选切换
             toggleAll(){
                 let url = this.allChecked ? '/carts/unSelectAll' : '/carts/selectAll';
@@ -92,7 +129,7 @@
                     this.renderData(res);
                 })
             },
-            // 数据渲染
+            // 数据渲染，公共赋值
             renderData(res){
                 this.list = res.cartProductVoList || [];
                 this.allChecked = res.selectedAll;
