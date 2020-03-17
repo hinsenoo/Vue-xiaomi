ef3884f9b35dab740749778fc190df02e296b73d
hinsenoo <837545254@qq.com>
2020-03-17 14:22:13 +0800

    支付宝支付对接

--- src/pages/aliPay.vue
+++ src/pages/aliPay.vue
@@ -1,10 +1,43 @@
 <template>
-    <div>
-        alipay
+    <div class="ali-pay">
+        <loading v-if="loading"></loading>
+        <div class="form" v-html="content">
+
+        </div>
     </div>
 </template>
 <script>
+    import Loading from './../components/Loading'
     export default {
         name: 'alipay',
+        components:{
+            Loading
+        },
+        data(){
+            return {
+                orderId: this.$route.query.orderNo,
+                // 返回的 html 源码
+                content: '',
+                loading: true, // 动画显示
+            }
+        },
+        mounted(){
+            this.paySubmit();
+        },
+        methods: {
+            paySubmit(){
+                this.axios.post('/pay',{
+                    orderId: this.orderId,
+                    orderName: 'Vue 高仿小米商城',
+                    amount: 0.01,   // 单位元
+                    payType: 1 // 1 支付宝 2 微信
+                }).then((res)=>{
+                    this.content = res.content;
+                    setTimeout(()=>{
+                        document.forms[0].submit();
+                    },100);
+                })
+            }
+        }
     }
 </script>
